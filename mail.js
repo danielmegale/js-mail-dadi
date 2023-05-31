7a8a6646a1b512799bd16a382c23548c45cf3c56
danielmegale <danim1@live.it>
2023-05-31 16:03:37 +0200

    first commit

---
+++ mail.js
@@ -0,0 +1,10 @@
+console.log('mail ok')
+//Prendo gli elementi dal DOM
+const button=document.getElementById('button');
+// Array emails
+const emailLog= ['pippo@gmail.com','pluto@gmail.com','paperino@gmail.com','topolino@gmail.com'];
+
+button.addEventListener('click' + function (){
+ const email=document.getElementById('email')
+ console.log(email)
+})
