6cc135978e67e42ca8aaf08c843164a34bb1a19a
danielmegale <danim1@live.it>
2023-05-31 17:38:55 +0200

    added mail + dice

--- dice.js
+++ dice.js
@@ -1,1 +1,19 @@
 console.log('dice ok')
+
+//Prendo gli elementi dal DOM
+const diceButton=document.getElementById('dice-button')
+const totalResult=document.getElementById('dice-result')
+const userResult=document.getElementById('user-dice')
+const pcResult=document.getElementById('pc-dice')
+diceButton.addEventListener('click', function(){
+    let userDice = Math.floor(Math.random() * 6)+1;
+    console.log(userDice)
+    userResult.innerText='Il tuo tiro: '+ userDice;
+    let pcDice =Math.floor(Math.random()* 6)+1
+    console.log(pcDice)
+    pcResult.innerText='Il tiro del pc: '+ pcDice
+    totalResult.innerText='You Lose'
+    if(userDice> pcDice){
+        totalResult.innerText='You Win'
+    }
+})
--- mail.js
+++ mail.js
@@ -1,10 +1,25 @@
 console.log('mail ok')
 //Prendo gli elementi dal DOM
 const button=document.getElementById('button');
+const result=document.getElementById('email-result');
 // Array emails
 const emailLog= ['pippo@gmail.com','pluto@gmail.com','paperino@gmail.com','topolino@gmail.com'];
 
-button.addEventListener('click' + function (){
- const email=document.getElementById('email')
- console.log(email)
+button.addEventListener('click',function (){
+    const mail=document.getElementById('email');
+    console.log(mail.value);
+    let isValid=false;
+    let userEmail='';
+    for(let i=0 ; i<emailLog.length ; i++){
+        userEmail=emailLog[i];
+        if(mail.value === userEmail){
+            isValid=true;
+        }
+ }
+    if(isValid === true){
+        result.innerText='Email valida';
+    }else{
+        result.innerText='Email non valida';
+    }
+    
 })
